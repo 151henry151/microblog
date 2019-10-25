598e7fb768bb2d20cf2e759cf8af627bfeebf8ef
Henry Romp <151henry151@gmail.com>
2019-10-25 14:04:55 -0400

    posts per page configuration

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['henry@romp.network']
+    POSTS_PER_PAGE = 3
