a7479d676f3ac720f4d75d84176f1bcfc377d2e9
Henry Romp <151henry151@gmail.com>
2019-10-25 14:11:16 -0400

    requests

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    page = requests.args.get('page', 1, type=int)
+    page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
     return render_template('index.html', title='Home', form=form, posts=posts.items)
