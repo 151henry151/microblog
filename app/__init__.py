2da8ab054a2ef845df0bc444a90e5bc9f8433b31
Henry Romp <151henry151@gmail.com>
2019-11-06 17:34:33 -0500

    flask-babel instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_moment import Moment
+from flask_babel import Babel
 import logging
 from flask_bootstrap import Bootstrap
 import os
@@ -19,6 +20,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
