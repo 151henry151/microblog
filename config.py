7860bf4d01db95e3eca98e0601d93d24b41df900
Henry Romp <151henry151@gmail.com>
2019-11-06 19:15:31 -0500

    Revert "synchronize with tutorial files for chapter 13"
    This reverts commit c8b044d872a170bd22835bc738fd373888194468.

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -9,9 +8,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
-    LANGUAGES = ['en', 'es']
+    ADMINS = ['henry@romp.network']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en_US', 'es']
