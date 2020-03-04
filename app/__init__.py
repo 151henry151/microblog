13538689e3c6159c9221984a77d6fa8f77061f75
Henry Romp <151henry151@gmail.com>
2020-03-04 12:06:02 -0500

    Elasticsearch

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from elasticsearch import Elasticsearch
 from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,6 +9,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
 from config import Config
 
 db = SQLAlchemy()
@@ -34,7 +34,6 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
-
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
 
