7dfa80a4b8ae3f193d63ec62e66910738e1ae2a9
Henry Romp <151henry151@gmail.com>
2020-03-11 17:53:42 -0400

    fix QR code generation

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for, flash, session, request
+from flask import render_template, redirect, url_for, flash, session, request, abort
+from io import BytesIO
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
